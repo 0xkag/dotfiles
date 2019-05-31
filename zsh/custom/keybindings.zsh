5b36b8179ab2e4ecab0b525bffe505033ebb1cb2
Kyle George <kgeorge@tcpsoft.com>
2019-05-31 00:16:09 -0400

    readline (bash) & zsh: C-left/C-right, M-left/M-right

---
+++ zsh/custom/keybindings.zsh
@@ -0,0 +1,23 @@
+# keep this in sync with $DOTFILES/readline/inputrc
+
+bindkey "^[[5C" forward-word
+bindkey "^[[5D" backward-word
+bindkey "^[[1;5C" forward-word
+bindkey "^[[1;5D" backward-word
+bindkey "^[[3C" forward-word
+bindkey "^[[3D" backward-word
+bindkey "^[[1;3C" forward-word
+bindkey "^[[1;3D" backward-word
+bindkey "^[[C" forward-word
+bindkey "^[[D" backward-word
+bindkey "^[^[[C" forward-word
+bindkey "^[^[[D" backward-word
+bindkey "^[[OC" forward-word
+bindkey "^[[OD" backward-word
+bindkey "^[OC" forward-word
+bindkey "^[OD" backward-word
+bindkey "^[^[OC" forward-word
+bindkey "^[^[OD" backward-word
+bindkey "^[^[[OC" forward-word
+bindkey "^[^[[OD" backward-word
+
